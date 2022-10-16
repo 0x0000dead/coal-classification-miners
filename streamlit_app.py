1c02d0d5617266928250e1e1bccaf7bcceaf075c
v.d.olennikov <v.d.olennikov@tinkoff.ru>
2022-10-16 03:41:56 +0300

    add max_size graph

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,75 @@
 import json
 
 import cv2
+import pandas as pd
+import streamlit as st
+import altair as alt
+from matplotlib import pyplot as plt
 
 import constants
-from ores import Ores
 from Analyzer import Analyzer
-from visualize import draw_bboxes
+from ores import Ores
+from visualize import draw_bboxes, get_size
 
-import streamlit as st
+MAX_HIST_LEN = 5000
+
+
+def max_size(bboxes):
+    res = 0.0
+    for bbox in bboxes:
+        if bbox['size'] > res:
+            res = bbox['size']
+    return res
+
+
+def draw_max_size_hist(histogram_max_size, list_max_size, bboxes_list):
+    if len(list_max_size) >= MAX_HIST_LEN:
+        del list_max_size[0]
+    list_max_size.append(max_size(bboxes_list))
+
+    fig, ax = plt.subplots()
+    ax.plot(list_max_size)
+    ax.set_title('Max ores size, mm')
+    ax.set_xlabel("time in frames")
+    histogram_max_size.pyplot(fig)
+
+
+
+def bboxes_add_sizes(bboxes):
+    for bbox in bboxes:
+        bbox['size'] = get_size(bbox)
 
 
 def main():
     analyzer = Analyzer(model_path=constants.ROOT_DIR + r'/model/best.pt', mode_type='yolo')
-    cap = cv2.VideoCapture('resources/example.mp4')
+
+    list_max_sizes = []
+    df_percents = pd.DataFrame()
 
     st.title("Miners DataHack")
-    st.header("Camera:")
+    st.subheader("Camera:")
 
-    radio_place = st.sidebar.empty()
-    selected_class = radio_place.radio("Select Class", list(Ores), 4,
-                                       format_func=lambda o: o.description)
+    # # left menu
+    selected_class = st.sidebar.radio("Select Class", list(Ores), 4, format_func=lambda o: o.description)
+    flag_one_size = st.sidebar.checkbox("Only this size")
+    flag_draw_boxes = st.sidebar.checkbox("Draw bboxes", True)
 
-    imageLocation = st.empty()
+    image_location = st.empty()
+    histogram_max_size = st.empty()
+
+    # 0 for web camera
+    cap = cv2.VideoCapture('resources/example.mp4')
     while cap.isOpened():
         ret, frame = cap.read()
 
         bboxes_list = json.loads(analyzer.get_bboxes(frame))
-        img = draw_bboxes(frame, bboxes_list, selected_class)
+        bboxes_add_sizes(bboxes_list)
 
-        imageLocation.image(img)
+        if flag_draw_boxes:
+            frame = draw_bboxes(frame, bboxes_list, selected_class, flag_one_size)
+        image_location.image(frame, channels="BGR")
+
+        draw_max_size_hist(histogram_max_size, list_max_sizes, bboxes_list)
 
     cap.release()
     cv2.destroyAllWindows()
--- visualize.py
+++ visualize.py
@@ -1,30 +1,32 @@
 import cv2
 
-from constants import CONVEYER_WIDTH, CONVEYER_BOTTOM_PX, CONVEYER_TOP_PX
+from constants import CONVEYER_WIDTH, CONVEYER_BOTTOM_PX, CONVEYER_TOP_PX, IMG_HIGH
 
-GREEN_COLOR = (0, 255, 0)
-RED_COLOR = (0, 0, 255)
-BLUE_COLOR = (255, 0, 0)
+LOW_SIZE_COLOR = (0, 255, 0)  # green
+BIG_SIZE_COLOR = (0, 0, 255)  # red
+NORMAL_SIZE_COLOR = (255, 255, 0)  # light blue
 BOX_COLOR = (255, 255, 255)
 
 
-def visualize_bbox(img, bbox, ore, img_high, thickness=3):
+def visualize_bbox(img, bbox, ore, one_size=False, thickness=3):
     x_min, y_min, x_max, y_max, = bbox['xmin'], bbox['ymin'], bbox['xmax'], bbox['ymax']
     x_min, x_max, y_min, y_max = int(x_min), int(x_max), int(y_min), int(y_max)
 
     # choose color
-    size = get_size(bbox, img_high)
+    size = bbox['size']
     if ore.max_size < 0:
-        color = GREEN_COLOR
+        color = LOW_SIZE_COLOR
     elif size >= ore.max_size:
-        color = RED_COLOR
+        color = BIG_SIZE_COLOR
     elif ore.min_size <= size < ore.max_size:
-        color = BLUE_COLOR
+        color = NORMAL_SIZE_COLOR
     else:
-        color = GREEN_COLOR
+        color = LOW_SIZE_COLOR
+
+    if color == LOW_SIZE_COLOR and one_size:
+        return img
 
     cv2.rectangle(img, (x_min, y_min), (x_max, y_max), color=color, thickness=thickness)
-    cv2.imshow("", img)
     return img
 
 
@@ -37,15 +39,14 @@ def base_size(bbox):
 
 
 # size of the bbox in mm
-def get_size(bbox, conveyer_len):
+def get_size(bbox, conveyer_len=IMG_HIGH):
     pix_to_mm = CONVEYER_WIDTH / CONVEYER_BOTTOM_PX
     ratio = 1.0 + int(bbox['ymax']) * (CONVEYER_BOTTOM_PX / CONVEYER_TOP_PX - 1.0) / conveyer_len
     return base_size(bbox) * pix_to_mm * ratio
 
 
-def draw_bboxes(image, bboxes, ore):
+def draw_bboxes(image, bboxes, ore, one_size=False):
     img = image.copy()
-    img_high = img.shape[0]
     for bbox in bboxes:
-        img = visualize_bbox(img, bbox, ore, img_high)
+        img = visualize_bbox(img, bbox, ore, one_size=one_size)
     return img
